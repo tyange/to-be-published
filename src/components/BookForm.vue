aad35b6cdf7c2b7a53037ef964ea1e3063cb3403
tyange <usun16@gmail.com>
2022-06-30 13:18:34 +0900

    entering text testing

---
+++ src/components/BookForm.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const enteredText = ref("");
+</script>
+<template>
+  <form>
+    <input type="text" v-model="enteredText" placeholder="testing text">
+  </form>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import BookList from "../components/BookList.vue";
+import BookForm from "@/components/BookForm.vue";
 </script>
 
 <template>
   <main class="w-full flex justify-center items-center">
     <div>
+      <BookForm />
       <BookList />
     </div>
   </main>
