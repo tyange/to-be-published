da3a2d6e84e2b3735b2632c55d99ab39495f3738
tyange <usun16@gmail.com>
2022-07-01 18:02:31 +0900

    book list styling

--- src/components/BookItem.vue
+++ src/components/BookItem.vue
@@ -12,7 +12,7 @@ const props = defineProps<{
 
 const convertedPrice = computed(() => {
   if (!isNaN(parseInt(props.pre_price))) {
-    return `${props.pre_price}원`;
+    return props.pre_price;
   } else {
     return "미정";
   }
@@ -22,44 +22,74 @@ const convertedAuthors = computed(() => {
   const splitAuthors = props.author.split(";");
 
   if (splitAuthors.length > 1) {
-    splitAuthors.pop()
+    splitAuthors.pop();
     return splitAuthors;
   }
 
   return null;
 });
+
+const convertedIsbn = computed(() => {
+  if (props.isbn.trim().length === 0) {
+    return "미정";
+  }
+
+  return props.isbn;
+});
+
+const convertedPreDate = computed(() => {
+  return `${props.pre_date.slice(0, 4)}-${props.pre_date.slice(4, 6)}-${props.pre_date.slice(6)}`;
+});
 </script>
 
 <template>
   <div>
-    <p>{{ title }}</p>
-    <p class="flex gap-2">
+    <div class="relative p-6">
+      <span class="absolute top-0 left-0 text-xs text-gray-300">
+        도서명
+      </span>
+      <p>
+        {{ title }}
+      </p>
+    </div>
+    <div class="relative p-6 flex gap-2">
+      <span class="absolute top-0 left-0 text-xs text-gray-300">
+        작가
+      </span>
       <span v-if="convertedAuthors === null">
         {{ author }}
       </span>
       <span v-for="convertedAuthor in convertedAuthors" :key="convertedAuthor">
         {{ convertedAuthor }};
       </span>
-    </p>
-    <p>
-      <span>{{ isbn }}</span>
-      <span>{{ publisher }}</span>
-    </p>
-    <p>
-      <span>
-        예상 가격:
-      </span>
-      <span>
-        {{ convertedPrice }}
-      </span>
-    </p>
-    <p>
-      <span>
-        출간예정일:
-      </span>
-      <span>
-        {{ pre_date }}
-      </span>
-    </p>
+    </div>
+    <div class="flex gap-5">
+      <div class="relative p-6 flex gap-2">
+        <span class="absolute top-0 left-0 text-xs text-gray-300">
+          ISBN
+        </span>
+        <span>{{ convertedIsbn }}</span>
+      </div>
+      <div class="relative p-6 flex gap-2">
+        <span class="absolute top-0 left-0 text-xs text-gray-300">
+          출판사
+        </span>
+        <span>{{ publisher }}</span>
+      </div>
+    </div>
+    <div class="flex gap-5">
+      <div class="relative p-6 flex gap-2">
+        <span class="absolute top-0 left-0 text-xs text-gray-300">
+          예정 가격(원)
+        </span>
+        <span>{{ convertedPrice }}</span>
+      </div>
+      <div class="relative p-6 flex gap-2">
+        <span class="absolute top-0 left-0 text-xs text-gray-300">
+          출간예정일
+        </span>
+        <span>{{ convertedPreDate }}</span>
+      </div>
+    </div>
   </div>
 </template>
--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -9,7 +9,7 @@ const books = computed(() => store.getBooks);
 </script>
 
 <template>
-  <ul class="w-full flex flex-col gap-3">
+  <ul class="w-full flex flex-col gap-12">
     <li v-for="book in books" :key="book.EA_ISBN" class="border rounded-xl w-full p-3 shadow-md">
       <BookItem :title="book.TITLE" :pre_price="book.PRE_PRICE" :author="book.AUTHOR" :pre_date="book.PUBLISH_PREDATE"
                 :isbn="book.EA_ISBN" :publisher="book.PUBLISHER" />
