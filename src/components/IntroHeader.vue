51ed9edb17d32f47ecc4bf0b4d261bed9d8bec69
tyange <usun16@gmail.com>
2022-07-03 12:39:45 +0900

    added intro header

---
+++ src/components/IntroHeader.vue
@@ -0,0 +1,26 @@
+<template>
+  <details open="true">
+    <summary>
+      <p class="text-xl italic">
+        출간예정도서 검색기
+        <span class="text-xs not-italic hidden text-gray-400">
+          (클릭하면 접힙니다)
+        </span>
+      </p>
+    </summary>
+    <p class="mt-3">
+      국립중앙도서관의 서지 정보 Open API를 이용한 출간예정도서 검색기입니다.
+    </p>
+  </details>
+</template>
+
+<style>
+details summary::marker {
+  display: none;
+  content: "";
+}
+
+details[open] summary p span {
+  display: inline;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
+import IntroHeader from "@/components/IntroHeader.vue";
 import BookList from "../components/BookList.vue";
 import BookForm from "@/components/BookForm.vue";
 </script>
 
 <template>
+  <header class="w-full flex justify-center items-center h-20">
+    <IntroHeader/>
+  </header>
   <main class="w-full flex justify-center items-center">
-    <div class="w-1/2 flex flex-col items-center gap-10 pt-10">
+    <div class="w-1/2 flex flex-col items-center gap-10">
       <BookForm />
       <BookList />
     </div>
