568aa4280f0a7da76c951599340e4231259791d2
tyange <usun16@gmail.com>
2022-07-06 13:39:23 +0900

    add footer

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const currentYear = new Date().getFullYear();
+</script>
+<template>
+  <footer class="flex justify-center">
+    <p class="flex items-center justify-center text-sm h-10 gap-1">
+      <span>&copy;</span>
+      <a href="https://github.com/tyange">tyange</a>
+      <span>{{currentYear}}</span>
+    </p>
+  </footer>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,6 +5,7 @@ import BookForm from "@/components/BookForm.vue";
 import PageButtons from "@/components/PageButtons.vue";
 import { computed } from "vue";
 import { useBooksStore } from "@/stores/books";
+import TheFooter from "@/components/TheFooter.vue";
 
 const store = useBooksStore();
 const pageSize = import.meta.env.VITE_PAGE_SIZE;
@@ -12,22 +13,23 @@ const pageSize = import.meta.env.VITE_PAGE_SIZE;
 
 const pageCounts = computed(() => {
   const dividedTotalCount = store.totalCount / pageSize;
-  if(store.totalCount % pageSize === 0) {
+  if (store.totalCount % pageSize === 0) {
     return dividedTotalCount;
   }
   return Math.floor(dividedTotalCount) + 1;
-})
+});
 </script>
 
 <template>
-  <header class="w-full flex justify-center items-center h-48">
-    <IntroHeader/>
+  <header class="w-full flex justify-center items-center h-48 mt-5">
+    <IntroHeader />
   </header>
   <main class="w-full flex justify-center items-center">
     <div class="flex w-full p-2 md:p-0 md:w-auto flex-col items-center gap-10">
       <BookForm />
       <BookList />
-      <PageButtons v-if="store.totalCount > 0" :page-counts="pageCounts"/>
+      <PageButtons v-if="store.totalCount > 0" :page-counts="pageCounts" />
     </div>
   </main>
+  <TheFooter />
 </template>
