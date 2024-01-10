28cb78893e51c0e4e5e7c0720050e75a6a242d33
tyange <usun16@gmail.com>
2024-01-11 06:47:16 +0900

    add books data

---
+++ src/data/books.ts
@@ -0,0 +1,65 @@
+import type { Publisher } from '../types/publisher'
+
+import munhakdongneLogoImg from '../assets/logos/munhak-dongne.jpg'
+import changboLogoImg from '../assets/logos/chang-bi.jpg'
+import minumsaLogoImg from '../assets/logos/minumsa.jpg'
+import munjiLogoImg from '../assets/logos/munji.jpg'
+import hyundaimunhakLogoImg from '../assets/logos/hyndai-munhak.jpg'
+import achimdalLogoImg from '../assets/logos/achimdal.jpg'
+import hangyureLogoImg from '../assets/logos/hangyure.jpg'
+import zaummoumLogoImg from '../assets/logos/zaum-moum.jpg'
+import zakgajungsinLogoImg from '../assets/logos/zakga-jungsin.jpg'
+import eunhangnamuLogoImg from '../assets/logos/eunhang-namu.jpg'
+
+export const booksData: Publisher[] = [
+  {
+    publisherName: '문학동네',
+    publisherImageSrc: munhakdongneLogoImg,
+    publisherDetailPageUrl: ''
+  },
+  {
+    publisherName: '창비',
+    publisherImageSrc: changboLogoImg,
+    publisherDetailPageUrl: ''
+  },
+  {
+    publisherName: '민음사',
+    publisherImageSrc: minumsaLogoImg,
+    publisherDetailPageUrl: ''
+  },
+  {
+    publisherName: '문학과지성사',
+    publisherImageSrc: munjiLogoImg,
+    publisherDetailPageUrl: ''
+  },
+  {
+    publisherName: '현대문학',
+    publisherImageSrc: hyundaimunhakLogoImg,
+    publisherDetailPageUrl: ''
+  },
+  {
+    publisherName: '아침달',
+    publisherImageSrc: achimdalLogoImg,
+    publisherDetailPageUrl: ''
+  },
+  {
+    publisherName: '한겨레출판',
+    publisherImageSrc: hangyureLogoImg,
+    publisherDetailPageUrl: ''
+  },
+  {
+    publisherName: '자음과모음',
+    publisherImageSrc: zaummoumLogoImg,
+    publisherDetailPageUrl: ''
+  },
+  {
+    publisherName: '작가정신',
+    publisherImageSrc: zakgajungsinLogoImg,
+    publisherDetailPageUrl: ''
+  },
+  {
+    publisherName: '은행나무',
+    publisherImageSrc: eunhangnamuLogoImg,
+    publisherDetailPageUrl: ''
+  }
+]
