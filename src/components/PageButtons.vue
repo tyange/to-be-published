aa49032a3e75d592f1a8976f71f33b74e69bfb5c
tyange <usun16@gmail.com>
2024-01-13 13:19:16 +0900

    add dots icon

--- src/components/PageButtons.vue
+++ src/components/PageButtons.vue
@@ -34,13 +34,37 @@ const pageNumbers = computed(() => {
   <div class="flex items-center justify-center my-10 text-gray-500">
     <template v-if="searchParams['page_no'] > 4">
       <page-button :page-num="1" />
-      <span class="mx-2"> </span>
+      <span class="mx-2">
+        <svg
+          class="w-6 h-6 text-gray-800 dark:text-white"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          viewBox="0 0 16 3"
+        >
+          <path
+            d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z"
+          />
+        </svg>
+      </span>
     </template>
     <div class="flex gap-2">
       <PageButton v-bind:key="pageNum" v-for="pageNum in pageNumbers" :page-num="pageNum" />
     </div>
     <template v-if="searchParams['page_no'] < Number(pageCounts) - 3">
-      <span class="mx-2"> </span>
+      <span class="mx-2">
+        <svg
+          class="w-6 h-6 text-gray-800 dark:text-white"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          viewBox="0 0 16 3"
+        >
+          <path
+            d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z"
+          />
+        </svg>
+      </span>
       <PageButton :page-num="Number(pageCounts)" />
     </template>
   </div>
